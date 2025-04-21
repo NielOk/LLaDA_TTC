70510d4533dc58145e1e8adfeb4472184f490e13
Niel Ok <nielok@stanford.edu>
2025-04-21 15:43:35 -0700

    added extra step proportion sampling to mcts over policies

--- meta_cognition/MCTS/monte_carlo_tree_search.py
+++ meta_cognition/MCTS/monte_carlo_tree_search.py
@@ -43,7 +43,7 @@ def sample_partial_decoding_policy(cur_decoding_policy_state=None, possible_temp
         cur_step = cur_decoding_policy_state["step_id"]
         block_end_step_id = cur_decoding_policy_state["block_end_step_id"]
 
-    # Sample a temperature
+    # Sample a new partial policy state
     if cur_step < steps:
         # Sample a temperature from the possible temperatures
         temperature = random.choice(possible_temperatures)
@@ -53,19 +53,22 @@ def sample_partial_decoding_policy(cur_decoding_policy_state=None, possible_temp
         remasking_strategy = random.choice(possible_remasking_strategies)
         cur_decoding_policy_state["remasking_strategy_schedule"].append(remasking_strategy)
 
-        # Sample a block length if eligible
+        # Sample a block length and extra step proportion if eligible
         if cur_step == block_end_step_id and block_id < max_num_blocks:
             blocks_remaining = max_num_blocks - block_id
 
             # Sample a block length from the possible block lengths
             if blocks_remaining == 1: # Scenario where we only have 1 block remaining when time to sample a new block
                 block_length = gen_length - cur_step
+                extra_step_proportion = round(1 - sum(cur_decoding_policy_state["extra_step_proportions"]), 2)
             else: # Scenario where we have multiple blocks remaining
                 block_length = random.randint(1, gen_length - cur_step - (blocks_remaining - 1))
+                extra_step_proportion = round(random.uniform(0, 1 - sum(cur_decoding_policy_state["extra_step_proportions"])), 2)
 
             # Update decoding policy state
             cur_decoding_policy_state["block_end_step_id"] = cur_step + block_length
             cur_decoding_policy_state["block_schedule"].append(block_length)
+            cur_decoding_policy_state["extra_step_proportions"].append(extra_step_proportion)
             cur_decoding_policy_state["block_id"] += 1
 
     else:
