58d07bf055923f98f63722f4c36d2fec01837b91
Niel Ok <nielok@stanford.edu>
2025-04-30 00:46:18 -0700

    function definition of generating with decoding policy

--- meta_cognition/MCTS/monte_carlo_tree_search.py
+++ meta_cognition/MCTS/monte_carlo_tree_search.py
@@ -7,15 +7,29 @@ import argparse
 import random
 
 from transformers import AutoTokenizer, AutoModel
-
 from decoding_policy_state import DecodingPolicyState
+
+@ torch.no_grad()
+def generate_with_decoding_policy(model, prompt, decoding_policy, steps=128, gen_length=128, block_length=128, cfg_scale=0., mask_id=126336):
+    '''
+    Args:
+        model: Mask predictor.
+        prompt: A tensor of shape (1, L).
+        decoding_policy: Decoding policy state object.
+        steps: Sampling steps, less than or equal to gen_length.
+        gen_length: Generated answer length.
+        block_length: Block length, less than or equal to gen_length. If less than gen_length, it means using semi_autoregressive remasking.
+        cfg_scale: Unsupervised classifier-free guidance scale.
+        mask_id: The token id of [MASK] is 126336.
+    '''
+    pass
 
 
 def main():
     cur_decoding_policy_state = None
     possible_temperatures = [i / 10 for i in range(0, 11)]
     possible_remasking_strategies= ["low_confidence", "random"]
-    steps = 128
+    steps = 256
     gen_length = 128
     max_num_blocks = 4
     decoding_policy_state = DecodingPolicyState()
